349dd0b9fdebc4711225b55eca57ac146b0e507e
Ryniu <marcin3001@10g.pl>
2021-02-10 21:33:30 +0100

    Auto generate map id

--- pet-clinic-data/src/main/java/pl/ryniu/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/pl/ryniu/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package pl.ryniu.petclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import pl.ryniu.petclinic.model.BaseEntity;
 
-	protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Number> {
+
+	protected Map<Long, T> map = new HashMap<>();
 
 	Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -16,17 +19,32 @@ public abstract class AbstractMapService<T, ID> {
 	T findById(ID id) {
 		return map.get(id);
 	}
-	
-	T save(ID id, T object) {
-		map.put(id, object);
+
+	T save(T object) {
+		if (object != null) {
+			if (object.getId() == null) {
+				object.setId(getNextId());
+			}
+			map.put(object.getId(), object);
+		} else {
+			throw new RuntimeException("Object cannot be null");
+		}
 		return object;
 	}
-	
+
 	void deleteById(ID id) {
 		map.remove(id);
 	}
-	
+
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+
+	private Long getNextId() {
+		if (map.keySet().isEmpty()) {
+			return 1L;
+		} else {
+			return Collections.max(map.keySet()) + 1;
+		}
 	}
 }
